bc401eebcde861b0e4c1ad9339fef163dad85800
BlackOmegaSF <kian.leiner@gmail.com>
2022-03-06 15:09:17 -0800

    Update assembly info

--- PaperWorldConverter/Properties/AssemblyInfo.cs
+++ PaperWorldConverter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PaperWorldConverter")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("©2022 Kian Leiner")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
