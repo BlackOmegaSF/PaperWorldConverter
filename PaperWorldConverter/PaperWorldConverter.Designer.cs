b4b85372dd02623bf45f7b3b18d883266403f7d5
BlackOmegaSF <kian.leiner@gmail.com>
2022-03-06 11:09:41 -0800

    Clean up labels

--- PaperWorldConverter/PaperWorldConverter.Designer.cs
+++ PaperWorldConverter/PaperWorldConverter.Designer.cs
@@ -167,7 +167,7 @@
             // 
             this.lblOutputLocation.AutoSize = true;
             this.lblOutputLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOutputLocation.Location = new System.Drawing.Point(9, 188);
+            this.lblOutputLocation.Location = new System.Drawing.Point(12, 188);
             this.lblOutputLocation.Name = "lblOutputLocation";
             this.lblOutputLocation.Size = new System.Drawing.Size(84, 16);
             this.lblOutputLocation.TabIndex = 4;
